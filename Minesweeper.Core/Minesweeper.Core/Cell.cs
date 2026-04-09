7d9e16f5177c9d4b8af7f79a05149fb7fcfd0d2b
Devin Holt <HOLTDW1@ETSU.EDU>
2026-04-08 23:10:20 -0400

    Cell logic

---
+++ Minesweeper.Core/Minesweeper.Core/Cell.cs
@@ -0,0 +1,38 @@
+﻿namespace Minesweeper.Core
+{
+    internal class Cell
+    {
+        /// <summary>
+        /// If the cell contains a mine
+        /// </summary>
+        public bool isMine;
+        /// <summary>
+        /// If the cell has been revealed
+        /// </summary>
+        public bool isRevealed;
+        /// <summary>
+        /// If the cell has a flag on it
+        /// </summary>
+        public bool isFlagged;
+        /// <summary>
+        /// The number of mines adjacent to the cell
+        /// </summary>
+        public int adjacentMines;
+        public override string ToString()
+        {
+            if (isFlagged)
+            {
+                return "F";
+            }
+            if (!isRevealed)
+            {
+                return "#";
+            }
+            if (isMine)
+            {
+                return "b";
+            }
+            else return ".";
+        }
+    }
+}
